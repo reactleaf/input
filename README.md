45de2c427c624f1081802747dad272ed7b2fb81f
양현석 <fourwingsy@gmail.com>
2023-06-18 19:40:49 +0900

    chore: prepare publish

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Variable Input components
 
 [See on Storybook](https://main--6447d5ba8b8f8cd5016c6166.chromatic.com/)
 
-### TextInput
+### WIP
 
-### NumberInput
+- styled-components 떼기
--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export { default as TextInput } from "./src/components/TextInput"
+export { default as NumberInput } from "./src/components/NumberInput"
+export { default as FileInput } from "./src/components/FileInput"
