c62065d384661660e81cc75f5baebcfe1b234bf7
YG <yg@medility.ai>
2023-12-31 22:28:12 +0900

    feat: missing d.ts file

---
+++ src/hookform.ts
@@ -0,0 +1,4 @@
+export { default as FileInput } from "./components/FileInput/hookform"
+export { default as NumberInput } from "./components/NumberInput/hookform"
+export { default as TextInput } from "./components/TextInput/hookform"
+export { default as PasswordInput } from "./components/PasswordInput/hookform"
--- src/hookform.tsx
+++
@@ -1,4 +0,0 @@
-export { default as FileInput } from "@/components/FileInput/hookform"
-export { default as NumberInput } from "@/components/NumberInput/hookform"
-export { default as TextInput } from "@/components/TextInput/hookform"
-export { default as PasswordInput } from "@/components/PasswordInput/hookform"
