9b1795a09b5372f2c009956741fb4ca8be9fa434
YG <yg@medility.ai>
2023-12-31 19:16:27 +0900

    fix: awww

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TextInput } from "@/components/TextInput"
-export { default as NumberInput } from "@/components/NumberInput"
-export { default as FileInput } from "@/components/FileInput"
--- src/@types/styled-components.d.ts
+++
@@ -1,6 +0,0 @@
-import "styled-components"
-import { Theme } from "@reactleaf/theme"
-
-declare module "styled-components" {
-  export interface DefaultTheme extends Theme {}
-}
